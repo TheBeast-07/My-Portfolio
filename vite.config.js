d63ba8156dfdb97540f1000615912ddcb9f9f696
unknown <ahtashampervaiz6@gmail.com>
2025-02-23 08:37:04 -0800

    Fix profile image path for production

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/your-repository-name/',  // Add this line to specify the base path
 })
+
