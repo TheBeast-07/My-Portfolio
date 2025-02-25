f85d062daa2f0f627299698b07301d7153dd9d3a
unknown <ahtashampervaiz6@gmail.com>
2025-02-24 23:08:16 -0800

    some changes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,28 +1,62 @@
 import React from "react";
-import skills from "./data/skills.json";
 
 const Skills = () => {
+  const skillsData = [
+    {
+      title: "HTML",
+      imageSrc: "assets/skills/html.png"
+    },
+    {
+      title: "CSS",
+      imageSrc: "assets/skills/css.png"
+    },
+    {
+      title: "JavaScript",
+      imageSrc: "assets/skills/js.png"
+    },
+    {
+      title: "React",
+      imageSrc: "assets/skills/React.webp"
+    },
+    {
+      title: "Node.js",
+      imageSrc: "assets/skills/node.png"
+    },
+    {
+      title: "Express",
+      imageSrc: "assets/skills/express.png"
+    },
+    {
+      title: "GitHub",
+      imageSrc: "assets/skills/github.png"
+    },
+    {
+      title: "MongoDB",
+      imageSrc: "assets/skills/mongo.png"
+    },
+    {
+      title: "Figma",
+      imageSrc: "assets/skills/figma.png"
+    }
+  ];
+
   return (
-    <>
-      <div className="container skills" id="skills">
-        <h1>SKILLS</h1>
-        <div className="items">
-          {skills.map((data) => (
-            <>
-              <div
-                className="item"
-                key={data.id}
-                data-aos="flip-left"
-                data-aos-duration="1000"
-              >
-                <img src={`/assets/${data.imageSrc}`} alt="" />
-                <h3>{data.title}</h3>
-              </div>
-            </>
-          ))}
-        </div>
+    <div className="container skills" id="skills">
+      <h1>SKILLS</h1>
+      <div className="items">
+        {skillsData.map((skill, index) => (
+          <div
+            className="item"
+            key={index}
+            data-aos="flip-left"
+            data-aos-duration="1000"
+          >
+            <img src={skill.imageSrc} alt={skill.title} />
+            <h3>{skill.title}</h3>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
